62b7bb347005cf6b8e4e7f54e53021a2867ad95d
carlpascual1818 <carlpascual1818@gmail.com>
2026-02-01 23:03:44 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js - Update this with your deployed Apps Script Web App URL
 
-const API_URL = 'YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyCPK3PPhZl-mOScuLgAj-E1InNDSfm45gtVU4n1NffM97Yc0516K7Ym2hoPaUfqmk/exec';
 
 // After deploying backend to Apps Script:
 // 1. Copy the Web App URL
